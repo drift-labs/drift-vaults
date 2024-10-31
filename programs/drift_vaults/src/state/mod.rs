42ea6e8758090db3c2fbcab24521383d3b7c7820
John Rieck <jackdrieck@gmail.com>
2024-10-31 06:20:26 -0400

    export state module

--- programs/drift_vaults/src/state/mod.rs
+++ programs/drift_vaults/src/state/mod.rs
@@ -8,10 +8,10 @@ pub use withdraw_unit::*;
 
 pub mod account_maps;
 pub mod events;
-mod tokenized_vault_depositor;
+pub mod tokenized_vault_depositor;
 pub mod traits;
-mod vault;
-mod vault_depositor;
-mod vault_protocol;
+pub mod vault;
+pub mod vault_depositor;
+pub mod vault_protocol;
 pub mod withdraw_request;
-mod withdraw_unit;
+pub mod withdraw_unit;
